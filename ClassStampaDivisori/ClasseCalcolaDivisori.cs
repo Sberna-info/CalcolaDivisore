d317d0a50d7a9d861427b8a4d4d35c4518a7016e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-30 11:08:25 +0100

    Add project files.

---
+++ ClassStampaDivisori/ClasseCalcolaDivisori.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStampaDivisori
+{
+    public class ClasseCalcolaDivisori
+    {
+        public static int LeggiNum()
+        {
+            Console.Write("Inserisci un numero: ");
+            int n = int.Parse(Console.ReadLine());
+            return n;
+        }
+        public static void StampaDivisori(int n)
+        {
+            int MAX = n / 2;
+            int i = 1;
+            if (n > 0)
+            {
+                while (i <= MAX)
+                {
+                    if (n % i == 0)
+                        Console.WriteLine($"Il numero {i} è un divisore di {n}");
+                    i++;
+                }
+                Console.WriteLine($"Il numero {n} è un divisore di {n}");
+            }
+            else
+            {
+                Console.WriteLine("Il numero inserito non è valido.");
+            }
+        }
+    }
+}
---
+++ ProgramDivisori/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassStampaDivisori;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramDivisori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = ClasseCalcolaDivisori.LeggiNum();
+            ClasseCalcolaDivisori.StampaDivisori(n);
+            Console.ReadLine();
+        }
+    }
+}
